9330e2b1ac939b885c6feb0872237138fdd99940
Michael Mayo <greenisus@gmail.com>
2010-04-22 17:24:07 -0700

    flavor request constructor

--- iPhone/CTSlicehostFlavorRequest.h
+++ iPhone/CTSlicehostFlavorRequest.h
@@ -7,10 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CTSlicehostRequest.h"
 
+@class CTSlicehostFlavor;
 
-@interface CTSlicehostFlavorRequest : NSObject {
+@interface CTSlicehostFlavorRequest : CTSlicehostRequest {
 
 }
+
+// GET https://apikey@api.slicehost.com/flavors.xml
++ (id)listRequest;
+- (NSArray *)flavors;
+
+// GET https://apikey@api.slicehost.com/flavors/xxxx.xml
++ (id)getFlavorRequest:(NSUInteger)flavorId;
+- (CTSlicehostFlavor *)flavor;
 
 @end
--- iPhone/CTSlicehostFlavorRequest.m
+++ iPhone/CTSlicehostFlavorRequest.m
@@ -7,8 +7,42 @@
 //
 
 #import "CTSlicehostFlavorRequest.h"
+#import "CTSlicehostFlavor.h"
 
 
 @implementation CTSlicehostFlavorRequest
+
+#pragma mark -
+#pragma mark Constructors
+
++ (id)flavorRequestWithMethod:(NSString *)method path:(NSString *)path {
+	NSString *urlString = [NSString stringWithFormat:@"%@%@", [CTSlicehostRequest host], path];	
+	CTSlicehostFlavorRequest *request = [[[CTSlicehostFlavorRequest alloc] initWithURL:[NSURL URLWithString:urlString]] autorelease];
+	[request addBasicAuthenticationHeaderWithUsername:[CTSlicehostRequest apiKey] andPassword:@""];
+	[request setRequestMethod:method];
+	return request;
+}
+
+#pragma mark -
+#pragma mark GET Requests
+
+// GET https://apikey@api.slicehost.com/flavors.xml
++ (id)listRequest {
+    return nil;
+}
+
+- (NSArray *)flavors {
+    return nil;
+}
+
+// GET https://apikey@api.slicehost.com/flavors/xxxx.xml
++ (id)getFlavorRequest:(NSUInteger)flavorId {
+    return nil;
+}
+
+- (CTSlicehostFlavor *)flavor {
+    return nil;
+}
+
 
 @end
